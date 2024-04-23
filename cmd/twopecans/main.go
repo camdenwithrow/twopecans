70e62ce329671c60c10e8555431c574a81bbb23b
Camden Withrow <withrowcamden@gmail.com>
2024-04-23 18:18:14 -0500

    setup echo

--- cmd/twopecans/main.go
+++ cmd/twopecans/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+
+	"github.com/camdenwithrow/twopecans/templates"
+)
 
 func main() {
-	fmt.Println("Hello")
+	e := echo.New()
+
+	e.GET("/hello", HomeHandler)
+
+	e.Logger.Fatal(e.Start(":4321"))
+}
+
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	ctx.Response().Writer.WriteHeader(statusCode)
+	ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+	return t.Render(ctx.Request().Context(), ctx.Response().Writer)
+}
+
+func HomeHandler(c echo.Context) error {
+	return Render(c, http.StatusOK, templates.Home())
 }
